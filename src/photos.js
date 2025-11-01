569254538eb367bc120e2550e8efe8b13fa39150
Rosa Kosol <kosolrosa@gmail.com>
2025-11-01 11:11:07 +0800

    Fixed specified dimensions in photo file

--- src/photos.js
+++ src/photos.js
@@ -2,8 +2,8 @@
 const photos = [
   {
     src: "/photos/image01.jpg",
-    width: 2400,
-    height: 1734,
+    width: 2048,
+    height: 1365,
     alt: "Hiking boots",
     title: "Whispers of the Forest",
     description: "Captured in the Blue Mountains, 2023",
@@ -11,14 +11,14 @@ const photos = [
   },
   {
     src: "/photos/image02.jpg",
-    width: 2400,
-    height: 3600,
+    width: 1365,
+    height: 2048,
     alt: "Purple petaled flowers near a mountain",
   },
   {
     src: "/photos/image03.jpg",
-    width: 2400,
-    height: 1600,
+    width: 2048,
+    height: 1365,
     alt: "A person pointing at a beige map",
   },
 ]
